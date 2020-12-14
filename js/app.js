597da3b7a69468ab2508547b4bf6e4d2d5ca749e
Seppe De Langhe <seppedelanghe17@gmail.com>
2020-12-14 15:13:23 +0100

    split scss and add api js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,39 @@
+// Fixer.io API
+const api_key = '73efda1fbe5e9eafe4b8878d0cc87bce';
+const base_uri = 'http://data.fixer.io/api/';
+
+const base_curr = 'EUR';
+const curr_options = [
+    'USD',
+    'GBP',
+    'AUD',
+    'CAD',
+    'JPY',
+];
+
+const instance = axios.create({
+    baseURL: 'http://data.fixer.io/api/',
+    timeout: 5000,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+const getLatest = (curr = base_curr, symbols = curr_options) => {
+    instance.get('/latest', {
+        params: {
+            'access_key': api_key,
+            'symbols': symbols.join(),
+            'format': 1,
+        }
+    }).then(response => {
+        console.log(response);
+    }).catch(err => {
+        console.error(err);
+    });
+}
+
+
 // Define a new component called todo-item
 Vue.component('floating-bubble', {
     props: [
@@ -7,12 +43,11 @@ Vue.component('floating-bubble', {
     ],
     template: `
     <div :class="'fx-rise fx-rise--' + Math.round(size / 2) + ' fx-circle size-w-' + size + ' size-h-' + size "></div>`
-})
+});
 
 var app = new Vue({
     el: '#app',
     data: {
-        message: 'Hello World',
         bubbles: [
             { size: 2 },
         ]
@@ -22,14 +57,7 @@ var app = new Vue({
             let h = node.offsetHeight;
             let w = node.offsetWidth;
             let top = window.innerHeight + h;
-            
-            // Set left to max 20% from sides
             let sideOffset = window.innerWidth * Math.random();
-            /*
-            if (Math.random() > 0.5 ) { // random true or false 50/50
-                sideOffset = window.innerWidth - sideOffset;
-            }
-            */
 
             node.style.left = `${sideOffset}px`;
             node.style.bottom = `-${h}px`;
@@ -39,50 +67,40 @@ var app = new Vue({
                 node.style.opacity = 0;
             }, 200);
         },
+        initRise() {
+            var riseEl = document.getElementsByClassName('fx-rise');
+            for (let i = 0; i < riseEl.length; i++) {
+                this.startRise(riseEl.item(i));
+            }
+        },
+        addBubbles() {
+            let bubbleDelay = 750;
+
+            window.setInterval(() => {
+                this.bubbles.push({
+                    size: Math.floor(Math.random() * 6) + 1,
+                });
+    
+                window.setTimeout(() => {
+                    let addedBubble = document.querySelector('.fx-rise:last-child');
+                    this.startRise(addedBubble);
+                }, 10);
+            }, bubbleDelay); // add bubbles based on size of screen
+        },
+
 
     },
     mounted() {
-        let learnMore = document.getElementById('learnMore');
-        learnMore.addEventListener('click', () => {
-            let top = document.getElementById('about').offsetTop;
-            window.scrollTo({
-                top: top,
-                left: 0,
-                behavior: 'smooth',
-            });
-        });
-
-        document.addEventListener('scroll', (event) => {
-            //console.log(event);
-        });
-
-        var riseEl = document.getElementsByClassName('fx-rise');
-        for (let i = 0; i < riseEl.length; i++) {
-            this.startRise(riseEl.item(i));
-        }
-
-        let bubbleDelay = Math.round(window.innerWidth * 2) / 2; // increase to do more
-        bubbleDelay = 750;
-
-        console.log(bubbleDelay);
-
-        window.setInterval(() => {
-            this.bubbles.push({
-                size: Math.floor(Math.random() * 6) + 1,
-                //speed: Math.ceil(Math.random() * 6),
-            });
-
-            window.setTimeout(() => {
-                let addedBubble = document.querySelector('.fx-rise:last-child');
-                this.startRise(addedBubble);
-            }, 10);
-        }, bubbleDelay); // add bubbles based on size of screen
+        this.initRise();
+        this.addBubbles();
+        
+        
     },
 });
 
 
-// Chart
-
+// Charts
+/*
 var ctx = document.getElementById('exampleChart').getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'line',
@@ -113,3 +131,5 @@ var myChart = new Chart(ctx, {
         },
     }
 });
+
+*/
